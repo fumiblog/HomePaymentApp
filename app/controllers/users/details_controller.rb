391e30f59619606005eaaea0050bbb9592c31b14
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-05-24 11:22:40 +0900

    [user detail_index] 3-7

--- app/controllers/users/details_controller.rb
+++ app/controllers/users/details_controller.rb
@@ -11,6 +11,26 @@ class Users::DetailsController < ApplicationController
     redirect_to root_path
   end
 
+  def index
+    if params[:category_id] == nil
+      @details = current_user.details.order(day: 'DESC')
+    else
+      @details = current_user.details.where(:category_id => params[:category_id])
+    end
+  end
+
+  def edit
+    @detail = Detail.find(params[:id])
+    @category = current_user.categories
+  end
+
+  def update
+    @detail = Detail.find(params[:id])
+    @detail.user_id = current_user.id
+    @detail.update!(detail_params)
+    redirect_to users_details_path
+  end
+
   def destroy
     @detail = Detail.find(params[:id])
     @detail.destroy
